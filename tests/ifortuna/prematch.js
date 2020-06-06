b9171c0e6ac65894daa90d5a5f38ffea344247bb
Radim Daniel Panek <rdpanek@gmail.cz>
2020-06-06 21:54:05 +0200

    ifortuna/prematch: added

---
+++ tests/ifortuna/prematch.js
@@ -0,0 +1,39 @@
+const title = 'Ifortuna.cz | Fortuna Game'
+
+describe('Fortuna Prematch', () => {
+  before(() => {
+    if (process.env.PT_AUDIT == 'allow') {
+      browser.enablePerformanceAudits()
+    }
+  })
+
+  describe('HomePage', () => {
+      it('open', () => {
+        browser.CoverageStart()
+        browser.url('/')
+        const titleElm = $(`//title[contains(text(),"${title}")]`)
+        browser.waitForloadEventEnd()
+        expect(titleElm.waitForExist(undefined, false, "Element title not found. The page couldn't be loaded in time.")).to.be.true
+        browser.TakeCoverage('fortuna')
+        browser.EvaluatePerformanceAudit()
+      });
+  });
+
+  describe('Premach', () => {
+      it('open', () => {
+        const elmPrematch = $('//a[@href="/sazeni"][contains(string(),"Sporty")]')
+        elmPrematch.click()
+
+        const prematchTitleElm = $('//title[contains(text(),"Kurzové sázení online | Fortuna")]')
+        browser.waitForloadEventEnd()
+        expect(prematchTitleElm.waitForExist(undefined, false, "Element title not found. The page couldn't be loaded in time.")).to.be.true
+        browser.EvaluatePerformanceAudit()
+      });
+  });
+
+  after(() => {
+    if (process.env.PT_AUDIT == 'allow') {
+      browser.disablePerformanceAudits()
+    }
+  })
+})

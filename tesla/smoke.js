662bd0e9574c7efdd41da67a64648d20a356975e
Radim Daniel Panek <rdpanek@gmail.cz>
2020-07-31 16:44:30 +0200

    tesla/smoke.js: added Coverage

--- tesla/smoke.js
+++ tesla/smoke.js
@@ -2,11 +2,13 @@ describe('Smoke monitoring on tesla.com', () => {
 
   describe('HomePage', () => {
       it('open', () => {
+        browser.CoverageStart()
         browser.url('https://www.tesla.com/')
         const title = 'Electric Cars, Solar & Clean Energy | Tesla'
         const titleElm = $(`//title[contains(text(),"${title}")]`)
         browser.waitForloadEventEnd()
         expect(titleElm.waitForExist({timeoutMsg: "Element title not found. The page couldn't be loaded in time."})).to.be.true
+        browser.TakeCoverage()
       });
   });
 })

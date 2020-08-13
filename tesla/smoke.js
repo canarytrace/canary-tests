deb93ad22760c719179401593eceba09b36e0fc2
Radim Daniel Panek <rdpanek@gmail.cz>
2020-08-13 12:00:52 +0200

    tesla/smoke.js: added hero elements

--- tesla/smoke.js
+++ tesla/smoke.js
@@ -9,15 +9,40 @@ before(() => {
 })
 
   describe('HomePage', () => {
-      it('open', () => {
-        browser.CoverageStart()
-        browser.url('https://www.tesla.com/')
-        const title = 'Electric Cars, Solar & Clean Energy | Tesla'
-        const titleElm = $(`//title[contains(text(),"${title}")]`)
-        browser.waitForloadEventEnd()
-        browser.EvaluatePerformanceAudit()
-        expect(titleElm.waitForExist({timeoutMsg: "Element title not found. The page couldn't be loaded in time."})).to.be.true
-        browser.TakeCoverage('tesla')
-      });
+    it('inject hero element measurement', function () {
+
+      // configuration
+      const configuration = {
+        patterns: [{
+          urlPattern: 'https://www.tesla.com*',
+          resourceType: 'Document',
+          requestStage: 'Response'
+        }]
+      }
+    
+      // rules
+      const rules = [
+        ['<head>', '<script>performance.mark(\'HE-start-head\')</script>\n<head>'],
+        ['</head>', '<head>\n<script>performance.mark(\'HE-stop-head\')</script>'],
+        ['<body', '<script>performance.mark(\'HE-start-body\')</script><body'],
+        ['</body>', '</body>\n<script>performance.mark(\'HE-stop-body\')</script>'],
+        ['<dialog id="locale-modal"', '<script>performance.mark(\'HE-start-dialog\')</script>\n<dialog id="locale-modal"'],
+        ['</dialog>', '<script>performance.mark(\'HE-stop-dialog\')</script>\n</dialog>'],
+      ]
+    
+      browser.InjectHeroElements(configuration, rules)
+    
+    });
+
+    it('open', () => {
+      browser.CoverageStart()
+      browser.url('https://www.tesla.com/')
+      const title = 'Electric Cars, Solar & Clean Energy | Tesla'
+      const titleElm = $(`//title[contains(text(),"${title}")]`)
+      browser.waitForloadEventEnd()
+      browser.EvaluatePerformanceAudit()
+      expect(titleElm.waitForExist({timeoutMsg: "Element title not found. The page couldn't be loaded in time."})).to.be.true
+      browser.TakeCoverage('tesla')
+    });
   });
 })
